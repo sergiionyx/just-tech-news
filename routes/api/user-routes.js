69f68023daf9ed244666fdd5b0d009e77a49e71d
sergii-onyx <twilight.cympak@gmail.com>
2022-05-10 11:40:22 -0400

    added upvote method

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { User } = require('../../models');
+const { User, Post, Vote } = require("../../models");
 
 // GET /api/users
 router.get('/', (req, res) => {
@@ -17,7 +17,21 @@ router.get('/', (req, res) => {
 // GET /api/users/1
 router.get('/:id', (req, res) => {
     User.findOne({
-        attributes: { exclude: ['password'] },
+        attributes: {
+            exclude: ['password'],
+            include: [
+                {
+                    model: Post,
+                    attributes: ['id', 'title', 'post_url', 'created_at']
+                },
+                {
+                    model: Post,
+                    attributes: ['title'],
+                    through: Vote,
+                    as: 'voted_posts'
+                }
+            ]
+        },
         where: {
             id: req.params.id
         }
