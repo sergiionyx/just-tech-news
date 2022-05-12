ea760a46fa341485aecd0e91fd5b2c0e4b2e9327
sergii-onyx <twilight.cympak@gmail.com>
2022-05-12 12:31:03 -0400

    code refactor, add plural check and cut url length

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const path = require('path');
 const express = require('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
+const helpers = require('./utils/helpers');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -18,14 +19,11 @@ const sess = {
     db: sequelize
   })
 };
+const hbs = exphbs.create({ helpers });
 
 app.use(session(sess));
-
-const hbs = exphbs.create({});
-
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
